75e934fac3c8c4b5c2c9cfacc142fdf9af2d16bd
JungWooJang0324 <chiang030204@gmail.com>
2022-02-04 22:10:28 +0900

    안녕하세요

---
+++ sistFirstPrj/src/Login/LoginModel.java
@@ -0,0 +1,15 @@
+package Login;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class LoginModel extends JFrame{
+	public LoginModel() {
+		System.out.println("내일 해야지");
+	}
+	
+	public static void main(String[] args) {
+		LoginModel lm = new LoginModel();
+	}
+	
+}
