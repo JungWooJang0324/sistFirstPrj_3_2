322006991fad3c2135573b9e3558fc04607843df
rkdtks619 <rkdtks619@gamil.com>
2022-02-06 23:57:15 +0900

    강산커밋

--- sistConsolPrj/src/Event/DialogEvent.java
+++ sistConsolPrj/src/Event/DialogEvent.java
@@ -7,12 +7,10 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -26,6 +24,9 @@ import View.LogDialog;
 public class DialogEvent extends WindowAdapter implements ActionListener {
 	private LogDialog ld;
 	private int first,last;
+	private int mode;
+	private int code200,code403,code404,code500;
+	private double code403pct,code500pct;
 	private String filePath, fileName;
 	private String[] langList = {"mongodb","res","ora","javascript", "java","hadoop","jsp","d8","jk9k","front" };
 	private int[] cnt = new int[langList.length];
@@ -46,9 +47,11 @@ public class DialogEvent extends WindowAdapter implements ActionListener {
 		//REPORT 버튼이 눌렸을 때
 		if(ae.getSource()==ld.getJbtnReport()) {
 			//아이디가 root면 오류메세지 생성
-			if(ld.getGroa().equals("관리자"))
+			if(ld.getGroa().equals("관리자")) {
 				JOptionPane.showMessageDialog(ld,"문서를 생성할 수 있는 권한이 없음");
 			}else {
+				JOptionPane.showMessageDialog(ld,"파일이 생성되었습니다.");
+				
 				//root 이외 사용자는 폴더 및 파일 생성
 				File folder=new File("c:/dev/report/");
 				folder.mkdirs();
@@ -68,7 +71,7 @@ public class DialogEvent extends WindowAdapter implements ActionListener {
 		    
 				}//catch
 			}//if else
-		
+		}//if
 		
 		
 		//LINE 버튼이 눌렸을 때
@@ -146,6 +149,39 @@ public class DialogEvent extends WindowAdapter implements ActionListener {
 		}
 		
 		
+	}
+	//4.최다 사용시간
+	public void hourCount(String fileContext) {
+		//로그 시간구해 배열에 저장
+		int hour[]= {Integer.parseInt(fileContext.substring(fileContext.lastIndexOf("[")+1, fileContext.lastIndexOf("]")).substring(11,13))};
+		int[] index= {};
+		int max=Integer.MIN_VALUE;
+		
+		//최다 시간 구하기
+		for(int i=0; i<hour.length;i++) {
+			index[hour[i]]++;
+		}
+		for(int i=0; i<index.length;i++) {
+			if(max<index[i]) {
+				max=index[i];
+				mode=i;
+			}
+		}
+	}
+	//3.5.6. 서비스 상태 횟수구하기
+	public void stateCount(String fileContext) {
+		int state= Integer.parseInt (fileContext.substring(fileContext.indexOf("[")+1, fileContext.indexOf("]")));
+		if(state==200) {
+			code200++;
+		}else if(state==403) {
+			code403++;
+		}else if(state==404) {
+			code404++;
+		}else if(state==500) {
+			code500++;
+		}
+		code403pct=(state*100)/code403;
+		code500pct=(state*100)/code500;
 	}
 
 }//class
